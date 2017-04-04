16c403b7a818932b61adc6fa1c437fea91ea3adf
Phoenix Project <phoenixproject.erp@gmail.com>
2017-04-04 00:41:49 -0300

    Aula 13 iniciada.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody2D rb2d;
+	[SerializeField]
+	private float maximo;
+	[SerializeField]
+	private float minimo;
+	[SerializeField]
+	private float velocidade;
+	[SerializeField]
+	private int tempoVida;
+	[SerializeField]
+	private int pontos;
+	[SerializeField]
+	private GameObject prefabExplosao1;
+	[SerializeField]
+	private GameObject prefabExplosao2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Movimentar()
+	{
+		//rb2d.
+	}
+}
