05c41c3507d1b114025b2ec9d1890d1696e3eab2
Phoenix Project <phoenixproject.erp@gmail.com>
2017-04-14 23:07:18 -0300

    Aula 14 completa - Falta corrigir a explosão do asteroide.

---
+++ Assets/Scripts/Instanciador.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GerarObjetos
+{
+	public GameObject prefab;
+	public int contador;
+	public float iniciarTempo;
+	public float iniciarEsperar;
+	public float tempo;
+}
+
+public class Instanciador : MonoBehaviour {
+
+	public GerarObjetos objetos;
+	public Vector2 instanciarValores;
+	// Use this for initialization
+	void Start () {
+		instanciarValores = CameraPrincipal.CoordenadaCamera();
+		StartCoroutine(InstanciarObjetos());
+	}
+	
+
+	IEnumerator InstanciarObjetos()
+	{
+		yield return new WaitForSeconds(objetos.iniciarTempo);
+
+		while (true)
+		{
+			for (int i = 0; i < objetos.contador; i++)
+			{
+				Vector2 posicao = new Vector2(Random.Range(-instanciarValores.x, instanciarValores.x), instanciarValores.y);
+				Instantiate(objetos.prefab, posicao, Quaternion.identity);
+				yield return new WaitForSeconds(objetos.iniciarEsperar);
+			}
+
+			yield return new WaitForSeconds(objetos.tempo);
+		}
+	}
+}
