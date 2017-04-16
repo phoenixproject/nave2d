ea5f41cb10c2cfb481faea4b346361d8e0a5330e
Phoenix Project <phoenixproject.erp@gmail.com>
2017-04-16 13:12:16 -0300

    Aula 16 no início (com inimigo já criado).

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour {
+
+	[SerializeField]
+	private float velocidade;
+	[SerializeField]
+	private int vida;
+	[SerializeField]
+	private GameObject prefabExplosao1;
+	[SerializeField]
+	private GameObject prefabExplosao2;
+	[SerializeField]
+	private GameObject prefabBomba;
+	[SerializeField]
+	private GameObject prefabInstanciar;
+
+	[SerializeField]
+	private int minValor;
+	[SerializeField]
+	private int maxValor;
+
+	private int valorInimigo;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
